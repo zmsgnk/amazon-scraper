89c9e4dfe393b654fbdfd1bcb74eded222b419ce
zmsgnk <kngshimizu@gmail.com>
2014-08-16 22:09:57 +0900

    update scrape_amazon.py

--- scrape_amazon.py
+++ scrape_amazon.py
@@ -3,26 +3,25 @@
 
 from bs4 import BeautifulSoup
 import urllib2
+from openpyxl import Workbook
+from openpyxl.cell import get_column_letter
+from datetime import datetime
+import time
+import sys
 
 
-def make_soup(url):
+def scrape_bestseller(url): 
 	html = urllib2.urlopen(url).read().decode('shift-jis')
 	soup = BeautifulSoup(html)	
-	return soup
 
-
-def prettify_price(price):
-	price = price.replace(u'\uffe5 ', '')
-	price = price.replace(',', '')
-	price = int(price)
-	return(price)
-
-
-def scrape_bestseller(soup): 
 	result = []
-
 	for item in soup.findAll('div', {'class': 'zg_item_normal'}): 
 		tmp = []
+
+		## brand
+		brand = item.find('div', {'class': 'zg_byline'}).string
+		brand = brand.replace('\n', '')
+		tmp.append(brand)
 
 	    ## title 
 		zg_title = item.find('div', {'class': 'zg_title'})
@@ -33,11 +32,6 @@ def scrape_bestseller(soup):
 		link = zg_title.find('a').get('href')
 		link = link.replace('\n', '')
 		tmp.append(link)
-
-		## brand
-		brand = item.find('div', {'class': 'zg_byline'}).string
-		brand = brand.replace('\n', '')
-		tmp.append(brand)
 
 		## list price
 		try:
@@ -57,8 +51,44 @@ def scrape_bestseller(soup):
 	return result
 
 
-		
-		
-		
+def prettify_price(price):
+	price = price.replace(u'\uffe5 ', '')
+	price = price.replace(',', '')
+	price = int(price)
+	return(price)
 
+		
+if __name__ == '__main__':
+	
+	categories = ["electronics", "food-beverage", "hpc", "automotive", 
+	              "beauty", "shoes", "sports", "computers", "baby", 
+	              "appliances", "apparel", "watch"]
 
+	data = {}
+	for category in categories:
+		tmp = [["ブランド", "商品名", "URL", "参考価格", "価格"]]
+		for page_num in xrange(5):
+			url = "http://www.amazon.co.jp/gp/bestsellers/%s/#%s" % (category, str(page_num+1))
+			result = scrape_bestseller(url)
+			tmp.extend(result)
+			time.sleep(1)
+		data[category] = tmp
+
+	## write xlsx
+	wb = Workbook()
+	ws = wb.worksheets[0]
+	for category in categories:
+		ws.title = category
+		for i, item_list in enumerate(data[category]):
+			for j, item in enumerate(item_list):
+				if j == 2 and i != 0:
+					ws.cell(row=i+1, column=j+1).hyperlink = item
+				else:
+					ws.cell(row=i+1, column=j+1).value = item
+		ws = wb.create_sheet()
+
+	today = datetime.today()
+	path_to_file = sys.argv[1]
+	fname = path_to_file + "amazon-bestseller_%s.xlsx" % today.strftime("%Y%m%d")
+
+	wb.save(filename=fname)
